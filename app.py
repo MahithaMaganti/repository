240ab6636a8974aea368c98de45417abdf99f553
MahithaMaganti <22251a1215@gmail.com>
2025-12-03 11:40:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def submit():
     username=request.form['username']
     email=request.form['email']
     year=request.form['year']
-    return render_template('result.html',name=username,email=email,year=year)
+    return render_template('register.html',name=username,email=email,year=year)
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
